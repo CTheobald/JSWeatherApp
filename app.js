3a97ac3dfd78e40bb2f7775d322c5a2ae83f971e
CTheobald <christinetheobald@icloud.com>
2020-04-21 20:05:47 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+    loadingText.style.display = 'block';
+    weatherBox.style.display = 'none';
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a City Name');
+    }
+    var http = new XMLHttpRequest();
+    // register for your own Open Weather API key and list below
+    var apiKey = 'OpenWeatherAPIkey';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+            var data = JSON.parse(http.responseText);
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            updateWeather(weatherData);
+            console.log(weatherData);
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong!');
+        }
+    };
+    http.send();
+}
+
+function updateWeather(weatherData) {
+    weatherCity.textContent = weatherData.cityName;
+    weatherDescription.textContent = weatherData.description;
+    weatherTemperature.textContent = weatherData.temperature;
+
+
+    loadingText.style.display = 'none';
+    weatherBox.style.display = 'block';
+
+}
+
